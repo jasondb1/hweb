b355d3b21690b1adad3cf43d9149494e722c9e36
jasondb1 <jason@teamdeboer.ca>
2019-07-05 21:39:38 -0600

    LED class and ComponentOutput super class.

---
+++ controllers/ComponentOutput.js
@@ -0,0 +1,69 @@
+const Gpio = require('onoff').Gpio;
+
+class ComponentOutput {
+
+    constructor() {
+        this.name = null;
+        this.location = null;
+        this.value = null;
+        this.status = null;
+        this.pinNumber = null;
+        this.lowOn = false;
+        this.pin = null;
+        this.updateInterval = null;
+        //update = null;
+    }
+
+    on() {
+        if (this.lowOn) {
+            this.pin.writeSync(0);
+        } else {
+            this.pin.writeSync(1);
+        }
+
+        this.status = true;
+        this.value = true;
+    }
+
+    off() {
+        if (this.lowOn) {
+            this.pin.writeSync(1);
+        } else {
+            this.pin.writeSync(0);
+        }
+
+        this.status = false;
+        this.value = false;
+
+    }
+
+    updateValue() {
+        this.value = this.pin.readSync();
+        return this.value;
+    }
+
+    setPin(pin) {
+        this.pinNumber = pin;
+        this.pin = new Gpio(this.pinNumber, 'out');
+    }
+
+    setLowOn() {
+        this.lowOn = true;
+    }
+
+    start(interval = 30) {
+        this.updateInterval = interval;
+        this.update = setInterval(
+            function () {
+                this.updateValue();
+            },
+            (this.updateInterval * 1000)
+        );
+    }
+
+    stop() {
+        clearInterval(this.update);
+    }
+}
+
+module.exports =  ComponentOutput;
--- controllers/Led.js
+++ controllers/Led.js
@@ -1,73 +1,22 @@
+"use strict";
+const ComponentOutput = require('./ComponentOutput');
+//import ComponentOutput from './ComponentOutput';
+
 const Gpio = require('onoff').Gpio;
 
-class Led {
-    name = null;
-    value = null;
-    status = null;
-    pinNumber = null;
-    lowOn = false;
-    pin = null;
-    updateInterval = null;
-    update = null;
-    log = null;
+class Led extends ComponentOutput {
+    //log = null;
 
-    constructor(pin_number) {
+    constructor(pin_number, name = 'led', location = "unknown") {
+        super();
         this.pinNumber = pin_number;
+        this.name = name;
+        this.location = location;
+
         this.pin = new Gpio(this.pinNumber, 'out');
         this.value = this.pin.readSync();
-    }
-
-    on() {
-        if (this.lowOn) {
-            this.pin.writeSync(0);
-        } else {
-            this.pin.writeSync(1);
-        }
-
-        this.status = true;
-        this.value = true;
-    }
-
-    off() {
-        if (this.lowOn) {
-            this.pin.writeSync(1);
-        } else {
-            this.pin.writeSync(0);
-        }
-
-        this.status = false;
-        this.value = false;
-
-    }
-
-    updateValue() {
-        this.value = this.pin.readSync();
-        return this.value;
-    }
-
-    setPin(pin) {
-        this.pinNumber = pin;
-        this.pin = new Gpio(this.pinNumber, 'out');
-    }
-
-    setLowOn() {
-        this.lowOn = true;
-    }
-
-    start(interval) {
-        this.updateInterval = interval;
-        this.update = setInterval(
-            function () {
-                this.updateValue();
-            },
-            (this.updateInterval * 1000)
-        );
-    }
-
-    stop(){
-        clearInterval(this.update);
     }
 
 };
 
-module.exports =  Led;
+module.exports = Led;

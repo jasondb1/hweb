f181d7f8a2ac8d890422136a05b23cbd674be593
jasondb1 <jason@teamdeboer.ca>
2019-07-11 08:17:45 -0600

    database

--- controllers/components.js
+++ controllers/components.js
@@ -4,14 +4,14 @@ const GarageRelay = require('./GarageRelay.js');
 const Dht22 = require('./Dht22');
 const Arduino = require('./Arduino');
 const Relay = require('./Relay');
+const Database = require('./database');
 
-const sqlite3 = require('sqlite3').verbose();
-const Gpio = require('onoff').Gpio;
-const fs = require('fs');
-
-//Files
-const DB_FILEPATH = './db/homeWeb.db';
-const LOG_FILEPATH = 'log.csv';
+// const sqlite3 = require('sqlite3').verbose();
+// const fs = require('fs');
+//
+// //Files
+// const DB_FILEPATH = './db/homeWeb.db';
+// const LOG_FILEPATH = 'log.csv';
 
 //Set Raspberry Pi pins
 const LEDPIN = 4;
@@ -20,16 +20,10 @@ const RELAY2PIN = 23;
 let ARDUINO_I2C_ADDR = 0x08;
 const DHT22PIN = 17;
 
-const TEMPPIN = 17;
 const SAMPLEINTERVAL = 10; //interval in seconds to sample temperature and humidity
 
 
 //components
-//let component = {};
-
-// let LED = new Gpio(LEDPIN, 'out');
-// let RELAY1 = new Gpio(RELAY1PIN, 'out');
-// let RELAY2 = new Gpio(RELAY2PIN, 'out');
 let indicator = new Led(LEDPIN);
 let garageRelay = new GarageRelay(RELAY1PIN);
 let arduino = new Arduino(ARDUINO_I2C_ADDR);
@@ -42,6 +36,8 @@ class ComponentsCtrl {
         this.component = {};
         this.update = null;
         this.updateInterval = null;
+        this.database = new Database();
+
     }
 
     init() {
@@ -83,50 +79,58 @@ class ComponentsCtrl {
         //arduino.readSensor();
         //this.init();
 
-        let datetime = new Date();
-        fs.appendFile(
-            LOG_FILEPATH,
-            datetime.getTime() +
-            "," +
-            datetime.toLocaleDateString() +
-            "," +
-            datetime.toLocaleTimeString() +
-            "," +
-            this.component.temp_local.value +
-            "," +
-            this.component.humidity_local.value +
-            "\n", function (err) {
-            }
-        );
+        // let datetime = new Date();
+        // fs.appendFile(
+        //     LOG_FILEPATH,
+        //     datetime.getTime() +
+        //     "," +
+        //     datetime.toLocaleDateString() +
+        //     "," +
+        //     datetime.toLocaleTimeString() +
+        //     "," +
+        //     this.component.temp_local.value +
+        //     "," +
+        //     this.component.humidity_local.value +
+        //     "\n", function (err) {
+        //     }
+        // );
 
         //open database
         let log_sensors = ['temp_local', 'humidity_local', 'temp_remote0', 'humidity_remote0', 'presistor_remote0', 'ledIndicator', 'garageRelay', 'relay2'];
 
-        let db = new sqlite3.Database(DB_FILEPATH, (err) => {
-            if (err) {
-                console.error("[components.js] " + err.message);
-            } else {
-                //console.log('Connected to the homeWeb database.');
-            }
-        });
+        // let db = new sqlite3.Database(DB_FILEPATH, (err) => {
+        //     if (err) {
+        //         console.error("[components.js] " + err.message);
+        //     } else {
+        //         //console.log('Connected to the homeWeb database.');
+        //     }
+        // });
+        let data = [];
 
         for (let key of log_sensors) {
-            db.run("INSERT INTO sensor_data(timestamp, description, sensor, value) VALUES( ?, ?, ?, ?)", [datetime.getTime(), this.component[key].name, key, this.component[key].value],
-                (err) => {
-                    if (err) {
-                        console.error(err);
-                    }
-                }
-            );
+            data.push( {description: this.component[key].name,
+                sensor: key,
+                value: this.component[key].value,
+                location: this.component[key].value
+            });
+            // db.run("INSERT INTO sensor_data(timestamp, description, sensor, value) VALUES( ?, ?, ?, ?)", [datetime.getTime(), this.component[key].name, key, this.component[key].value],
+            //     (err) => {
+            //         if (err) {
+            //             console.error(err);
+            //         }
+            //     }
+            // );
         }
 
-        db.close((err) => {
-            if (err) {
-                return console.error(err.message);
-            } else {
-                //console.log('Close the db connection');
-            }
-        });
+        this.database.insert(data);
+
+        // db.close((err) => {
+        //     if (err) {
+        //         return console.error(err.message);
+        //     } else {
+        //         //console.log('Close the db connection');
+        //     }
+        // });
     }
 
     currentStatus() {
---
+++ controllers/database.js
@@ -0,0 +1,136 @@
+const sqlite3 = require('sqlite3').verbose();
+const fs = require('fs');
+
+const DB_FILEPATH = './db/homeWeb.db';
+const LOG_FILEPATH = 'log.csv';
+let table = 'sensor_data';
+
+class Database {
+
+    constructor() {
+        this.db = null;
+        this.table = table;
+    }
+
+    connect() {
+        this.db = new sqlite3.Database(DB_FILEPATH, (err) => {
+            if (err) {
+                console.error("[components.js] " + err.message);
+            } else {
+                //console.log('Connected to the homeWeb database.');
+            }
+        });
+
+    }
+
+    close() {
+        this.db.close((err) => {
+            if (err) {
+                return console.error(err.message);
+            } else {
+                //console.log('Close the db connection');
+            }
+        });
+    }
+
+    insert(data) {
+
+        this.connect();
+
+        this.db.run(`CREATE TABLE IF NOT EXISTS ${this.table} (` +
+            "timestamp INTEGER," +
+            "description TEXT" +
+            "sensor TEXT" +
+            "value TEXT" +
+            "location TEXT" +
+            ")");
+
+        let datetime = new Date();
+
+        for (let row of data) {
+            this.db.run(`INSERT INTO ${this.table}(timestamp, description, location, sensor, value) VALUES( ?, ?, ?, ?)`,
+                [datetime.getTime(), row.description, row.location, row.sensor, row.value],
+                (err) => {
+                    if (err) {
+                        console.error(err);
+                    }
+                }
+            );
+        }
+
+        this.close();
+    }
+
+
+    reset(sensor) {
+        this.connect();
+
+        let where;
+        if (sensor === 'all') {
+            where = '1'; //reset all
+        } else {
+            where = 'sensor = ' + sensor;
+        }
+
+        this.db.run((`DELETE FROM sensor_data WHERE ?`), [where],
+            (err) => {
+                if (err) {
+                    console.error(err);
+                }
+            }
+        );
+
+        this.close();
+
+    }
+
+    exportData(filter = '1', filename = LOG_FILEPATH) {
+
+        this.connect();
+        let datetime = new Date();
+
+        this.db.each(`SELECT * FROM ${this.table} WHERE ?`, [filter], function (err, row) {
+
+            if (err) {
+                throw err;
+            }
+
+            //rows.forEach(function (row) {
+            //console.log(row.id + ": " + row.info);
+            console.log(row);
+
+            fs.appendFile(
+                filename,
+                datetime.getTime() +
+                "," +
+                datetime.toLocaleDateString() +
+                "," +
+                datetime.toLocaleTimeString() +
+                "," +
+                row.description +
+                "," +
+                row.location +
+                "," +
+                row.sensor +
+                "," +
+                row.value +
+                "\n", function (err) {
+                }
+            );
+            //});
+        });
+
+        this.close();
+
+    }
+
+
+}
+
+exports.module = Database;
+
+
+
+
+
+

bcef91d4de4704ba2a03fa8bad73a7653d5cc072
jasondb1 <jason@teamdeboer.ca>
2019-07-13 08:27:09 -0600

    UI work

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,10 +2,15 @@ import React from 'react'
 
 const Header = (props) => {
     return (
-        <div className='header'>
-            <h1>homeWeb</h1>
-            <div>Status Icons</div>
-            <div>User dropdown</div>
+        <div className='header container'>
+            <div className='row'>
+                <h1>homeWeb</h1>
+                <div className='col-2'>
+                    Status Icons
+                    <span>User dropdown</span>
+                </div>
+
+            </div>
         </div>
     )
 };
